4669c5778586e2b5d3f82a1568e37a217f273aa7
caiquemiranda <caiquemirandas@gmail.com>
2025-02-08 22:53:48 -0300

    define and access database

--- src/app.py
+++ src/app.py
@@ -2,6 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/olamundo")
-def hello_world():
-    return "<h1>Olá mundo!<h1>"
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config = True)
+    app.config.from_maping(
+        SECRET_KEY = "dev",
+	DATABASE = os.path.join(app.instance_path, "mybank.sqlite"),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent = True)
+    else:
+        app.config.from_mapping(test_config)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+
+    return app

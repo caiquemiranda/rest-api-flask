ec363267147653edb72eeaeccb034941d2a03af3
caiquemiranda <caiquemirandas@gmail.com>
2025-02-11 22:07:02 -0300

    Refactor app.py to improve code organization and enhance model definitions

--- src/app.py
+++ src/app.py
@@ -1,34 +1,39 @@
 import os
+import click
+from datetime import datetime
+import sqlalchemy as sa
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-import sqlalchemy as sa
-import click
 from flask.cli import with_appcontext
-from datetime import datetime
 
 
 class Base(DeclarativeBase):
     pass
 
+
 db = SQLAlchemy(model_class=Base)
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
-    
+    username: Mapped[str] = mapped_column(
+        sa.String, unique=True, nullable=False)
+
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, username={self.username!r})"
+
 
 class Post(db.Model):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
     title: Mapped[str] = mapped_column(sa.String, nullable=False)
     body: Mapped[str] = mapped_column(sa.String, nullable=False)
-    created: Mapped[datetime] = mapped_column(sa.DateTime, default=sa.func.now())
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
     author_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
-    
+
     def __repr__(self) -> str:
         return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+
 
 @click.command('init-db')
 def init_db_command():
@@ -36,6 +41,7 @@ def init_db_command():
     with current_app.app_context():
         db.create_all()
     click.echo('Initialized the database.')
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)

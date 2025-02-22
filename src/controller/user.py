f5d7a497ff80587c78e5a8dea6ea06e82c3f7546
caiquemiranda <caiquemirandas@gmail.com>
2025-02-22 07:41:05 -0300

    Refactor update_user function to dynamically update user attributes based on request data

--- src/controller/user.py
+++ src/controller/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
+from sqlalchemy import inspect
 
 app = Blueprint('user', __name__, url_prefix='/users')
 
@@ -44,10 +45,12 @@ def update_user(user_id):
     user = db.get_or_404(User, user_id)
     data = request.json
     
-    if "username" in data:
-        user.username = data["username"]
-        db.session.commit()
-        
+    mapper = inspect(User)
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+    db.session.commit()
+            
     return {
         "id": user.id,
         "username": user.username,
